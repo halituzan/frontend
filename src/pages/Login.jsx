1a9812aaa9bc83f26448f74ff5229bbe7f6b9611
Halit Uzan <halit.uzan@gmail.com>
2022-09-17 19:04:13 +0300

    update

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -25,6 +25,8 @@ export default function Login() {
         },
         {
           withCredentials: true,
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
         }
       );
       if (data) {
